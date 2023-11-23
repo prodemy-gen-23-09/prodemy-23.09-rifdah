bb4ce1399764ca88261dff88ee8528b4ee7aa7f0
Rifdah Kamilah <rifkamilah@gmail.com>
2023-11-24 01:45:30 +0700

    add folder

---
+++ gen-23.09-tailwind-css/tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/**/*{html,js}'],
+  theme: {
+    screens: {
+      sm: '576px',
+      md: '768px',
+      lg: '992px',
+      xl: '1200px'
+    },
+    container: {
+      center: true,
+      padding: '1rem'
+    },
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('images/image.jpg')",
+      },
+      colors: {
+        'primary' : '#FD3D57'
+      },
+      fontFamily:{
+        Jost: "'Jost', sans-serif"
+      }
+    },
+  },
+  plugins: [],
+}
+
