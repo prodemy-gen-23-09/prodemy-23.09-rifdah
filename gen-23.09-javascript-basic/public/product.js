230aaa339bac4fede2090ba9b714670990e736ae
Rifdah Kamilah <rifkamilah@gmail.com>
2023-11-27 21:42:59 +0700

    updated

--- gen-23.09-javascript-basic/public/product.js
+++ gen-23.09-javascript-basic/public/product.js
@@ -1,5 +1,5 @@
 function redirectToProductDetail(productId, productName, productPrice, productImage) {
-  var productDetailUrl = "detail_product1.html?id=" + productId + "&name=" + encodeURIComponent(productName) + "&price=" + encodeURIComponent(productPrice) + "&image=" + encodeURIComponent(productImage);
+  var productDetailUrl = "previewproduct.html?id=" + productId + "&name=" + encodeURIComponent(productName) + "&price=" + encodeURIComponent(productPrice) + "&image=" + encodeURIComponent(productImage);
   window.location.href = productDetailUrl;
 }
 
@@ -10,6 +10,9 @@ var productName = urlParams.get("name");
 var productPrice = urlParams.get("price");
 var productImage = urlParams.get("image");
 
-document.getElementById("productName").innerText = productName;
-document.getElementById("productPrice").innerText = productPrice;
-document.getElementById("mainImage").src = productImage;
+// document.getElementById("productName").innerText = productName;
+// document.getElementById("productPrice").innerText = productPrice;
+// document.getElementById("mainImage").src = productImage;
+document.querySelector("#productName").innerText = productName;
+document.querySelector("#productPrice").innerText = productPrice;
+document.querySelector("#mainImage").src = productImage;
