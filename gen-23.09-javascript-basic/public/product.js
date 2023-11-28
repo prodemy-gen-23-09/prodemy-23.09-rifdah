eb43d1654410d923bcf31e46a81878bfc7cba190
Rifdah Kamilah <rifkamilah@gmail.com>
2023-11-28 08:26:47 +0700

    add folder javascript basic

--- gen-23.09-javascript-basic/public/product.js
+++ gen-23.09-javascript-basic/public/product.js
@@ -1,18 +1,26 @@
-function redirectToProductDetail(productId, productName, productPrice, productImage) {
-  var productDetailUrl = "previewproduct.html?id=" + productId + "&name=" + encodeURIComponent(productName) + "&price=" + encodeURIComponent(productPrice) + "&image=" + encodeURIComponent(productImage);
-  window.location.href = productDetailUrl;
+// function redirectToProductDetail(productId, productName, productPrice, productImage) {
+//   var productDetailUrl = "previewproduct.html?id=" + productId + "&name=" + encodeURIComponent(productName) + "&price=" + encodeURIComponent(productPrice) + "&image=" + encodeURIComponent(productImage);
+//   window.location.href = productDetailUrl;
+// }
+
+// // Function Untuk Menangkap Data Dari Halaman List Product
+// var urlParams = new URLSearchParams(window.location.search);
+// var productId = urlParams.get("id");
+// var productName = urlParams.get("name");
+// var productPrice = urlParams.get("price");
+// var productImage = urlParams.get("image");
+
+// // document.getElementById("productName").innerText = productName;
+// // document.getElementById("productPrice").innerText = productPrice;
+// // document.getElementById("mainImage").src = productImage;
+// document.querySelector("#productName").innerText = productName;
+// document.querySelector("#productPrice").innerText = productPrice;
+// document.querySelector("#mainImage").src = productImage;
+
+const row = document.querySelector('#product1');
+const itemDetail = documen.querySelector('.item-detail');
+
+itemDetail.onclick = (e) => {
+  row.style.display = 'flex';
+  e.preventDefault();
 }
-
-// Function Untuk Menangkap Data Dari Halaman List Product
-var urlParams = new URLSearchParams(window.location.search);
-var productId = urlParams.get("id");
-var productName = urlParams.get("name");
-var productPrice = urlParams.get("price");
-var productImage = urlParams.get("image");
-
-// document.getElementById("productName").innerText = productName;
-// document.getElementById("productPrice").innerText = productPrice;
-// document.getElementById("mainImage").src = productImage;
-document.querySelector("#productName").innerText = productName;
-document.querySelector("#productPrice").innerText = productPrice;
-document.querySelector("#mainImage").src = productImage;
