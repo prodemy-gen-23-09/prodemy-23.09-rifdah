20ae304e5befe97640530404e44f53eaedbacd3e
Rifdah Kamilah <rifkamilah@gmail.com>
2023-12-07 22:04:00 +0700

    edited

--- gen-23.09-react-fetch-api/src/App.jsx
+++ gen-23.09-react-fetch-api/src/App.jsx
@@ -15,7 +15,7 @@ function App() {
       <Nav />
       <Routes>
         <Route path="/" element={<ProductList />} />
-        <Route path="/home" element={<HomeWithSWR />} />
+        {/* <Route path="/home" element={<HomeWithSWR />} /> */}
         <Route path="/detail/:id" element={<ProductDetail />} />
       </Routes>
       <Footer />
